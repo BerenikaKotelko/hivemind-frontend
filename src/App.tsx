214274a623ac4ca99f52f791ece79858a54d7121
Jamie Lou <jlou711@googlemail.com>
2022-01-05 14:50:30 +0000

    stuff

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,20 @@ import { Routes, Route } from "react-router-dom";
 import { useState } from "react";
 
 function App() {
-  const [users, setUsers] = useState<string[]>([
+  // const [users, setUsers] = useState<string[]>([
+  //   "Matt Phan",
+  //   "Jamie Lou",
+  //   "Beri Kotelko",
+  //   "Grace Zaborski",
+  //   "Someones Name is Very Long",
+  // ]);
+  const users = [
     "Matt Phan",
     "Jamie Lou",
     "Beri Kotelko",
     "Grace Zaborski",
     "Someones Name is Very Long",
-  ]);
+  ];
   const [currentUser, setCurrentUser] = useState<string>("");
   return (
     <>
