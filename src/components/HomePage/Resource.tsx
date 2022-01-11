4e063e14e60101af57d71d73a9c04526e1a5a9a2
BeriKotelko <berenika@kotelko.com>
2022-01-11 12:55:57 +0000

    show more button now only appears if there are 4 or more comments. Formatted, linted, and typechecked

--- src/components/HomePage/Resource.tsx
+++ src/components/HomePage/Resource.tsx
@@ -220,31 +220,33 @@ function Resource({ resource, currentUser }: ResourceProps) {
                         </li>
                       ))}
                     </ul>
-                    <button
-                      className="comment-toggle btn btn-primary"
-                      type="button"
-                      data-bs-toggle="collapse"
-                      data-bs-target="#collapseExample"
-                      aria-expanded="false"
-                      aria-controls="collapseExample"
-                      onClick={() => setExpanded(true)}
-                    >
-                      Show more
-                      {/* chevron icon for button */}
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="16"
-                        height="16"
-                        fill="currentColor"
-                        className="bi bi-chevron-down"
-                        viewBox="0 0 16 16"
+                    {comments.length > 3 && (
+                      <button
+                        className="comment-toggle btn btn-primary"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#collapseExample"
+                        aria-expanded="false"
+                        aria-controls="collapseExample"
+                        onClick={() => setExpanded(true)}
                       >
-                        <path
-                          fillRule="evenodd"
-                          d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
-                        />
-                      </svg>
-                    </button>
+                        Show more
+                        {/* chevron icon for button */}
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          className="bi bi-chevron-down"
+                          viewBox="0 0 16 16"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+                          />
+                        </svg>
+                      </button>
+                    )}
                   </>
                 )}
                 {expanded && (
