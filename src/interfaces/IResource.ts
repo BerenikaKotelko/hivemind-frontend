3a40de8d2cb9f8e2867647966e230a33eabb6a25
Grace Zaborski <grace.zaborski@beamery.com>
2022-01-13 08:58:05 +0000

    Updating IResource interface

--- src/interfaces/IResource.ts
+++ src/interfaces/IResource.ts
@@ -1,10 +1,10 @@
 export interface IResource {
-  id: number;
+  id?: number;
   author_id: number;
   title: string;
   description: string;
   recommended: string;
   url: string;
   date_added: number;
-  likes: string;
+  content_type: string
 }
